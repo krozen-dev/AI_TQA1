b80888fd3ed3b23ac0394c22e916cd03d59ac9ed
_KroZen_ <hannnttter@gmail.com>
2024-09-15 21:33:13 +0300

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ai_tqa',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     package_data={'ai_tqa': ['data/my_model.h5', 'data/data.json']},
     install_requires=[
