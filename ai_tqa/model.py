0a4db81be47d5668b9cf8f399eef49f2efb67ca2
_KroZen_ <hannnttter@gmail.com>
2024-09-27 20:32:53 +0300

    Update

--- ai_tqa/model.py
+++ ai_tqa/model.py
@@ -4,9 +4,23 @@ import json
 import re
 import nltk
 from nltk.tokenize import word_tokenize
+import requests
 
 nltk.download('punkt')
 nltk.download('punkt_tab')
+
+
+def ocr_space(image, api_key, is_file=True, overlay=False, language='rus'):
+    payload = {'isOverlayRequired': overlay, 'apikey': api_key, 'language': language}
+
+    if is_file:
+        with open(image, 'rb') as f:
+            r = requests.post('https://api.ocr.space/parse/image', files={image: f}, data=payload)
+    else:
+        payload['url'] = image
+        r = requests.post('https://api.ocr.space/parse/image', data=payload)
+
+    return json.loads(r.content.decode())
 
 
 class TextEvaluator:
@@ -25,17 +39,20 @@ class TextEvaluator:
         self.tokenizer = tf.keras.preprocessing.text.Tokenizer()
         self.tokenizer.fit_on_texts(texts)
 
+
     def preprocess_text(self, text):
         text = re.sub(r'[^a-zA-Zа-яА-ЯёЁ\s]', '', text.capitalize())
         tokens = word_tokenize(text)
         tokens = [word for word in tokens if word.isalnum()]
         return tokens
 
+
     def evaluate_word(self, word):
         sequence = self.tokenizer.texts_to_sequences([word])
         padded_sequence = tf.keras.preprocessing.sequence.pad_sequences(sequence, maxlen=self.max_sequence_length)
         prediction = self.model.predict(padded_sequence)
         return prediction[0, 0]
+
 
     def evaluate_text(self, text, detail=False):
         preprocessed_input = self.preprocess_text(text)
@@ -48,3 +65,12 @@ class TextEvaluator:
             return prediction[0, 0], bad_words
         else:
             return prediction[0, 0]
+
+
+    def read_image(self, image_path, api_key, language='rus'):
+        ocr_result = ocr_space(image_path, api_key=api_key, language=language)
+        if ocr_result['OCRExitCode'] == 1:
+            extracted_text = ocr_result['ParsedResults'][0]['ParsedText']
+            return extracted_text
+        else:
+            raise ValueError(f"Ошибка при обработке изображения: {ocr_result['ErrorMessage']}")
